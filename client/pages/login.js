4f403e3ebc36b8c90033cd78ab4692b9022ec4ab
irsyadm99 <irsyad_m99@apps.ipb.ac.id>
2021-11-11 20:41:24 +0700

    slicing login register

--- client/pages/login.js
+++ client/pages/login.js
@@ -2,47 +2,38 @@ import Image from "next/image";
 
 function login() {
   return (
-    <div className="flex">
-      <section className="max-w-2xl">
-        <div className="my-60 mx-80 w-96">
-          <h1 className="text-5xl">Masuk</h1>
-          <form className="space-y-3 mt-10">
-            <p className="text-lg">Email</p>
-            <input
-              type="email"
-              className="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white 
-              text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 
+    <div className="h-screen w-screen flex items-center justify-center">
+      <div className="">
+        <h1 className="text-2xl font-bold mb-10">Masuk</h1>
+        <form action="" className="w-[22.5rem] space-y-3">
+          <p className="text-lg">Email</p>
+          <input
+            type="email"
+            className="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white 
+              text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-primary 
               focus:border-transparent"
-              placeholder="Your email"
-            />
-            <p className="text-lg">Kata Sandi</p>
-            <input
-              type="password"
-              className="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white 
-              text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 
+            placeholder="Your email"
+          />
+          <p className="text-lg">Kata Sandi</p>
+          <input
+            type="password"
+            className="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white 
+              text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-primary 
               focus:border-transparent"
-              placeholder="Your password"
-            />
-            <p className="text-md text-gray-400">Lupa password?</p>
-            <button className="px-4 py-2 text-white bg-primary font-semibold rounded-lg border-2 border-primary-hover w-full">
-              Masuk
-            </button>
-          </form>
-
-          <p>Tidak punya akun?</p>
+            placeholder="Kata sandi"
+          />
+          <p className="text-md text-gray-500">Lupa password?</p>
+          <button className="px-4 py-2 text-white bg-primary font-semibold rounded-lg border-2 border-primary-hover w-full">
+            Masuk
+          </button>
+        </form>
+        <div className="border-t-2 border-gray-400 mt-6">
+          <p className="my-5 text-center">Tidak punya akun?</p>
           <button className="px-4 py-2 text-primary bg-white border-2 border-primary-border font-semibold rounded-lg w-full">
             Buat
           </button>
         </div>
-      </section>
-      <section className="hidden lg:inline">
-        <Image
-          src="/loginimg.png"
-          layout="fill"
-          objectFit="contain"
-          objectPosition="right"
-        />
-      </section>
+      </div>
     </div>
   );
 }
--- client/pages/signup.js
+++ client/pages/signup.js
@@ -2,47 +2,59 @@ import Image from "next/image";
 
 function signup() {
   return (
-    <div className="flex">
-      <section className="max-w-xl">
-        <div className="my-60 mx-80 w-96">
-          <h1 className="text-5xl">Masuk</h1>
-          <form className="space-y-3 mt-10">
-            <p className="text-lg">Email</p>
-            <input
-              type="email"
-              className="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white 
-              text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 
+    <div className="h-screen w-screen flex items-center justify-center">
+      <div className="">
+        <h1 className="text-2xl font-bold mb-10">Daftar akun</h1>
+        <form action="" className="w-[22.5rem] space-y-3">
+          <p className="text-lg">Nama Lengkap</p>
+          <input
+            type="text"
+            className="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white 
+              text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-primary 
               focus:border-transparent"
-              placeholder="Your email"
-            />
-            <p className="text-lg">Kata Sandi</p>
-            <input
-              type="password"
-              className="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white 
-              text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 
+            placeholder="Nama lengkap"
+          />
+          <p className="text-lg">Email</p>
+          <input
+            type="email"
+            className="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white 
+              text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-primary 
               focus:border-transparent"
-              placeholder="Your password"
-            />
-            <p className="text-md text-gray-400">Lupa password?</p>
-            <button className="px-4 py-2 text-white bg-primary font-semibold rounded-lg border-2 border-primary-hover w-full">
-              Masuk
-            </button>
-          </form>
-
-          <p>Tidak punya akun?</p>
-          <button className="px-4 py-2 text-primary bg-white border-2 border-primary-border font-semibold rounded-lg w-full">
-            Buat
+            placeholder="Your email"
+          />
+          <p className="text-lg">Kata Sandi</p>
+          <input
+            type="password"
+            className="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white 
+              text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-primary 
+              focus:border-transparent"
+            placeholder="Kata sandi"
+          />
+          <p className="text-md text-gray-400">*minimal 8 karakter</p>
+          <p className="text-lg">Konfirmasi Kata Sandi</p>
+          <input
+            type="password"
+            className="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white 
+              text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-primary 
+              focus:border-transparent"
+            placeholder="Kata sandi"
+          />
+          <p className="text-sm text-center">
+            Dengan mengklik daftar, kamu menyetujui{" "}
+            <span className="text-primary underline cursor-pointer">
+              Persyaratan dan Ketentuan
+            </span>{" "}
+            website kami
+          </p>
+          <button className="px-4 py-2 text-white bg-primary font-semibold rounded-lg border-2 border-primary-hover w-full">
+            Daftar
           </button>
-        </div>
-      </section>
-      <section className="hidden lg:inline">
-        <Image
-          src="/loginimg.png"
-          layout="fill"
-          objectFit="contain"
-          objectPosition="right"
-        />
-      </section>
+        </form>
+        <p className="text-md font-semibold mt-8 text-center">
+          Punya akun?{" "}
+          <span className="text-primary underline cursor-pointer">Masuk</span>
+        </p>
+      </div>
     </div>
   );
 }
